d2100a6ae4e4e05f1e40b9b03a8d763523e1b5d4
Marc-Arzaga <81542824+Marc-Arzaga@users.noreply.github.com>
2023-03-30 15:12:13 -0700

    Added second ADC

--- ads1015.py
+++ ads1015.py
@@ -5,19 +5,24 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import socket
 
-# Initialize I2C bus and ADS1015 ADC
+# Initialize I2C bus and ADS1015 ADCs
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1015(i2c)
+ads1 = ADS.ADS1015(i2c, address=0x48)  # First ADS1015 at address 0x48
+ads2 = ADS.ADS1015(i2c, address=0x49)  # Second ADS1015 at address 0x49
 
 # Configure ADC channels
-channels = [AnalogIn(ads, ADS.P0),
-            AnalogIn(ads, ADS.P1),
-            AnalogIn(ads, ADS.P2),
-            AnalogIn(ads, ADS.P3)]
+channels = [AnalogIn(ads1, ADS.P0),
+            AnalogIn(ads1, ADS.P1),
+            AnalogIn(ads1, ADS.P2),
+            AnalogIn(ads1, ADS.P3),
+            AnalogIn(ads2, ADS.P0),
+            AnalogIn(ads2, ADS.P1),
+            AnalogIn(ads2, ADS.P2),
+            AnalogIn(ads2, ADS.P3)]
 
 # Initialize Ethernet socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 12345))  # Replace 12345 with the desired port number
+s.bind(('0.0.0.0', 12345))  # Replace 0.0.0.0 with IP address of laptop and 12345 with the desired port number
 s.listen(1)
 
 # Wait for connection from laptop
@@ -49,6 +54,7 @@ while True:
     # Wait before reading again to avoid overwhelming laptop with data
     time.sleep(0.1)
 
-# Close connection and ADC
+# Close connection and ADCs
 conn.close()
-ads.deinit()
+ads1.deinit()
+ads2.deinit()
