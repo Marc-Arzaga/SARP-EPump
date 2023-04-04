dcd3cd38cc206022d3e2af07448d09787d68ebcb
Marc-Arzaga <81542824+Marc-Arzaga@users.noreply.github.com>
2023-04-04 16:34:41 -0700

    Update newMotor.py

--- newMotor.py
+++ newMotor.py
@@ -1,37 +1,19 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
-# Setup GPIO pin for ESC signal
-ESC_PIN = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ESC_PIN, GPIO.OUT)
-
-# Set initial PWM value
-PWM_FREQ = 50
-ESC_PWM = GPIO.PWM(ESC_PIN, PWM_FREQ)
-ESC_PWM.start(0)
-
-# Set speed control parameters
-MIN_RPM = 0
-MAX_RPM = 10000
-STEP_RPM = 100
-
-# Function to convert RPM to PWM duty cycle
-def rpm_to_duty_cycle(rpm):
-    return (rpm - MIN_RPM) / (MAX_RPM - MIN_RPM) * 100
-
-# Wait for keyboard input
+ledpin = 12				# PWM pin connected to LED
+GPIO.setwarnings(False)			#disable warnings
+GPIO.setmode(GPIO.BOARD)		#set pin numbering system
+GPIO.setup(ledpin,GPIO.OUT)
+pi_pwm = GPIO.PWM(ledpin,1000)		#create PWM instance with frequency
+pi_pwm.start(0)				#start PWM of required Duty Cycle 
 while True:
-    char = input("Press 'u' for Up arrow key, 'd' for Down arrow key, or 'q' to quit: ")
-    if char == 'u' or char == 'U':  # Up arrow key
-        new_rpm = min(int(ESC_PWM.duty_cycle) + STEP_RPM, MAX_RPM)
-        ESC_PWM.ChangeDutyCycle(rpm_to_duty_cycle(new_rpm))
-    elif char == 'd' or char == 'D':  # Down arrow key
-        new_rpm = max(int(ESC_PWM.duty_cycle) - STEP_RPM, MIN_RPM)
-        ESC_PWM.ChangeDutyCycle(rpm_to_duty_cycle(new_rpm))
-    elif char == 'q' or char == 'Q':  # Quit
-        break
-
-# Clean up GPIO pins
-ESC_PWM.stop()
-GPIO.cleanup()
+    for duty in range(0,101,1):
+        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+        sleep(0.01)
+    sleep(0.5)
+    
+    for duty in range(100,-1,-1):
+        pi_pwm.ChangeDutyCycle(duty)
+        sleep(0.01)
+    sleep(0.5)
